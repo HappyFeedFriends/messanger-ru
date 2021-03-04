f92cb31ccd23b85402b0e1896fe6ea9cf72cf31e
HappyFeedFriends <31066449+HappyFeedFriends@users.noreply.github.com>
2021-03-04 13:54:30 +0500

    added adapted landing page

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -7,7 +7,7 @@ import {
 class MainPage extends React.Component{
     render(){
         return (
-            <div className="MainPage">
+            <div className="MainPage column">
                 <header className="row">
                     <a href="/" className="row headerLeftBlock">
                         <Logo />
@@ -18,8 +18,8 @@ class MainPage extends React.Component{
                     <div className="links">
                         <ul className="row">
                             <li><Link className="column" to="/" >Главная</Link></li>
-                            <li><Link className="column" to="/about" >О нас</Link></li>
-                            <li><Link className="column" to="/contacts" >Контакты</Link></li>
+                            <li className="HiddenPhone" ><Link className="column" to="/about" >О нас</Link></li>
+                            <li className="HiddenPhone" ><Link className="column" to="/contacts" >Контакты</Link></li>
                             <li><Link className="column" to="/signup" >Регистрация</Link></li>                            
                         </ul>
                     </div>
@@ -33,91 +33,92 @@ class MainPage extends React.Component{
                         <div className="ring small" /> 
                     </div>
                 </div>
-                <div className="waves">
-                    <svg height="100%" width="100%" id="bg-svg" viewBox="0 0 1440 700" xmlns="http://www.w3.org/2000/svg" className="transition duration-300 ease-in-out delay-150">
-                        <path d="M 0,700 C 0,700 0,350 0,350 C 59.62820512820514,331.98717948717945 119.25641025641028,313.97435897435895 210,343 C 300.7435897435897,372.02564102564105 422.6025641025641,448.08974358974365 497,462 C 571.3974358974359,475.91025641025635 598.3333333333334,427.66666666666663 680,371 C 761.6666666666666,314.33333333333337 898.0641025641025,249.24358974358975 996,236 C 1093.9358974358975,222.75641025641025 1153.4102564102564,261.35897435897436 1221,289 C 1288.5897435897436,316.64102564102564 1364.2948717948718,333.3205128205128 1440,350 C 1440,350 1440,700 1440,700 Z"  fill="#262D34" className="transition-all duration-300 ease-in-out delay-150"></path>
-                    </svg>
-                </div>
-                <div className="advantagesBlock column">
-                    <span>Десять причин использовать Shark</span>
-                    <div className="AdvantageMainContainer row" >
+                <div className="column">
+                    <div className="waves">
+                        <svg height="100%" width="100%" id="bg-svg" viewBox="0 0 1440 700" xmlns="http://www.w3.org/2000/svg" className="transition duration-300 ease-in-out delay-150">
+                            <path d="M 0,700 C 0,700 0,350 0,350 C 59.62820512820514,331.98717948717945 119.25641025641028,313.97435897435895 210,343 C 300.7435897435897,372.02564102564105 422.6025641025641,448.08974358974365 497,462 C 571.3974358974359,475.91025641025635 598.3333333333334,427.66666666666663 680,371 C 761.6666666666666,314.33333333333337 898.0641025641025,249.24358974358975 996,236 C 1093.9358974358975,222.75641025641025 1153.4102564102564,261.35897435897436 1221,289 C 1288.5897435897436,316.64102564102564 1364.2948717948718,333.3205128205128 1440,350 C 1440,350 1440,700 1440,700 Z"  fill="#262D34" className="transition-all duration-300 ease-in-out delay-150"></path>
+                        </svg>
+                    </div>
+                    <div className="advantagesBlock column">
+                        <span>Десять причин использовать Shark</span>
+                        <div className="AdvantageMainContainer row" >
 
-                        <div className="AdvantageContainer column">
-                            <div className="imageAdvantage">
-                                <img src="./images/1.gif" alt=""/>
+                            <div className="AdvantageContainer column">
+                                <div className="imageAdvantage">
+                                    <img src="./images/1.gif" alt=""/>
+                                </div>
+                                <span className="cardHeader" >Удобство</span>
+                                <span className="cardDescription"><span>Простой</span>, интуитивный интерфейс.</span>
                             </div>
-                            <span className="cardHeader" >Удобство</span>
-                            <span className="cardDescription"><span>Простой</span>, интуитивный интерфейс.</span>
-                        </div>
 
-                        <div className="AdvantageContainer column">
-                            <div className="imageAdvantage">
-                                <img src="./images/2.gif" alt=""/>
+                            <div className="AdvantageContainer column">
+                                <div className="imageAdvantage">
+                                    <img src="./images/2.gif" alt=""/>
+                                </div>
+                                <span className="cardHeader" >Любовь</span>
+                                <span className="cardDescription">Наша <span>команда</span> любит каждого своего пользователя, а также маленькие детали</span>
                             </div>
-                            <span className="cardHeader" >Любовь</span>
-                            <span className="cardDescription">Наша <span>команда</span> любит каждого своего пользователя, а также маленькие детали</span>
-                        </div>
 
-                        <div className="AdvantageContainer column">
-                            <div className="imageAdvantage">
-                                <img src="./images/3.gif" alt=""/>
+                            <div className="AdvantageContainer column">
+                                <div className="imageAdvantage">
+                                    <img src="./images/3.gif" alt=""/>
+                                </div>
+                                <span className="cardHeader" >Подарки</span>
+                                <span className="cardDescription">Отправляйте <span>подарки</span> своим друзьям и родственникам</span>
                             </div>
-                            <span className="cardHeader" >Подарки</span>
-                            <span className="cardDescription">Отправляйте <span>подарки</span> своим друзьям и родственникам</span>
-                        </div>
 
-                        <div className="AdvantageContainer column">
-                            <div className="imageAdvantage">
-                                <img src="./images/4.gif" alt=""/>
+                            <div className="AdvantageContainer column">
+                                <div className="imageAdvantage">
+                                    <img src="./images/4.gif" alt=""/>
+                                </div>
+                                <span className="cardHeader" >Общение</span>
+                                <span className="cardDescription"><span>Общайтесь</span> о чём угодно и с кем угодно</span>
                             </div>
-                            <span className="cardHeader" >Общение</span>
-                            <span className="cardDescription"><span>Общайтесь</span> о чём угодно и с кем угодно</span>
-                        </div>
 
-                        <div className="AdvantageContainer column">
-                            <div className="imageAdvantage">
-                                <img src="./images/5.gif" alt=""/>
+                            <div className="AdvantageContainer column">
+                                <div className="imageAdvantage">
+                                    <img src="./images/5.gif" alt=""/>
+                                </div>
+                                <span className="cardHeader" >Социальность</span>
+                                <span className="cardDescription">Групповые чаты до <span>200 000 участников</span> одновременно</span>
                             </div>
-                            <span className="cardHeader" >Социальность</span>
-                            <span className="cardDescription">Групповые чаты до <span>200 000 участников</span> одновременно</span>
-                        </div>
 
-                        <div className="AdvantageContainer column">
-                            <div className="imageAdvantage">
-                                <img src="./images/6.gif" alt=""/>
+                            <div className="AdvantageContainer column">
+                                <div className="imageAdvantage">
+                                    <img src="./images/6.gif" alt=""/>
+                                </div>
+                                <span className="cardHeader" >Печеньки</span>
+                                <span className="cardDescription">У нас есть вкусные <span>печеньки</span> для каждого</span>
                             </div>
-                            <span className="cardHeader" >Печеньки</span>
-                            <span className="cardDescription">У нас есть вкусные <span>печеньки</span> для каждого</span>
-                        </div>
-                        <div className="AdvantageContainer column">
-                            <div className="imageAdvantage">
-                                <img src="./images/7.gif" alt=""/>
+                            <div className="AdvantageContainer column">
+                                <div className="imageAdvantage">
+                                    <img src="./images/7.gif" alt=""/>
+                                </div>
+                                <span className="cardHeader" >Мощность</span>
+                                <span className="cardDescription">Мощные  <span>сервера</span> которые выдержут любоё ваше сообщение</span>
                             </div>
-                            <span className="cardHeader" >Мощность</span>
-                            <span className="cardDescription">Мощные  <span>сервера</span> которые выдержут любоё ваше сообщение</span>
-                        </div>
-                        <div className="AdvantageContainer column">
-                            <div className="imageAdvantage">
-                                <img src="./images/8.gif" alt=""/>
+                            <div className="AdvantageContainer column">
+                                <div className="imageAdvantage">
+                                    <img src="./images/8.gif" alt=""/>
+                                </div>
+                                <span className="cardHeader" >Бесконечность</span>
+                                <span className="cardDescription">Бесконечный обмен <span>сообщениями</span> с друзьями и родственниками из любой страны</span>
                             </div>
-                            <span className="cardHeader" >Бесконечность</span>
-                            <span className="cardDescription">Бесконечный обмен <span>сообщениями</span> с друзьями и родственниками из любой страны</span>
-                        </div>
-                        <div className="AdvantageContainer column">
-                            <div className="imageAdvantage">
-                                <img src="./images/9.gif" alt=""/>
+                            <div className="AdvantageContainer column">
+                                <div className="imageAdvantage">
+                                    <img src="./images/9.gif" alt=""/>
+                                </div>
+                                <span className="cardHeader" >Эволюция</span>
+                                <span className="cardDescription">Проект <span>эволюционирует</span> каждый день <span>специально для вас</span></span>
                             </div>
-                            <span className="cardHeader" >Эволюция</span>
-                            <span className="cardDescription">Проект <span>эволюционирует</span> каждый день <span>специально для вас</span></span>
-                        </div>
-                        <div className="AdvantageContainer column">
-                            <div className="imageAdvantage">
-                                <img src="./images/10.gif" alt=""/>
+                            <div className="AdvantageContainer column">
+                                <div className="imageAdvantage">
+                                    <img src="./images/10.gif" alt=""/>
+                                </div>
+                                <span className="cardHeader" >Акула</span>
+                                <span className="cardDescription">А также у нас есть <span>АКУЛА</span></span>
                             </div>
-                            <span className="cardHeader" >Акула</span>
-                            <span className="cardDescription">А также у нас есть <span>АКУЛА</span></span>
                         </div>
-
                     </div>
                 </div>
             </div>
