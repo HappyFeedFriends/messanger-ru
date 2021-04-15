a939ba5b4b8b8ae458460c7c96ab1ac305cdb6a1
HappyFeedFriends <31066449+HappyFeedFriends@users.noreply.github.com>
2021-04-15 17:59:44 +0500

    add friendlist + add dynamic upload users

--- front/src/redux/rootReducer.tsx
+++ front/src/redux/rootReducer.tsx
@@ -1,12 +1,18 @@
 import {combineReducers } from 'redux';
 import Cookies from 'universal-cookie';
 import { APP_DEFAULT, STORAGE_DEFAULT } from './default';
-import { AppRedux, APP_LOADING_STATE, APP_USER_INIT_STATE, APP_USER_UPDATE, APP_USER_UPDATE_LANG, APP_USER_UPDATE_MESSAGE_VISIBLE, StorageRedux, STORAGE_INIT, STORAGE_MESSAGES_ADDED, STORAGE_MESSAGES_INIT, STORAGE_USER_UPDATE, UpdateAppState, UpdateStorageState } from './types';
+import { AppRedux, APP_LOADING_STATE, APP_USER_INIT_STATE, APP_USER_UPDATE, APP_USER_UPDATE_CHANNELS, APP_USER_UPDATE_FRIENDLIST, APP_USER_UPDATE_LANG, APP_USER_UPDATE_MESSAGE_VISIBLE, StorageRedux, STORAGE_INIT, STORAGE_MESSAGES_ADDED, STORAGE_MESSAGES_INIT, STORAGE_USER_UPDATE, UpdateAppState, UpdateStorageState } from './types';
 
 function APPReducer(state = APP_DEFAULT,actions : UpdateAppState) : AppRedux{
     switch (actions.type) {
         case APP_LOADING_STATE:
             return {...state,AppLoading : actions.payload}
+        case APP_USER_UPDATE_FRIENDLIST:
+            return {...state,user : {...state.user,friendsList : actions.payload}}
+        case APP_USER_UPDATE_CHANNELS:
+            const channels = state.user.Channels
+            channels.push(...(Object.keys(actions.payload.storage).map(value => Number(value))))
+            return {...state,user : {...state.user, Channels : channels}}
         case APP_USER_INIT_STATE:
             return {...state,user : actions.payload}
         case APP_USER_UPDATE:
@@ -30,15 +36,19 @@ function StorageReducer(state = STORAGE_DEFAULT,actions : UpdateStorageState) : 
             const channels = state.channels
             channels[actions.payload.channelID].messages = actions.payload.messages
             return {...state,channels : channels}
+        case APP_USER_UPDATE_CHANNELS : 
+            return {...state,channels : {...state.channels,...actions.payload.storage}}
         case STORAGE_MESSAGES_ADDED:
             const channel = state.channels
+            channel[actions.payload.channelID].messages = channel[actions.payload.channelID].messages || []
             channel[actions.payload.channelID].messages.push(actions.payload.message)
             return {...state,channels : channel}
         case STORAGE_USER_UPDATE:
             const users = [...state.users]
             const index = users.findIndex(value  => value.id === actions.payload.id)
-            if (index == null){
-                return state;
+            if (index < 0){
+                users.push(actions.payload)
+                return {...state,users : users};
             }
             users[index] = {...users[index],...actions.payload}
             return {...state, users : users}
